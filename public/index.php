f34c31462508776e47731832b7be39101f3e3f28
sandeshsmartguy <sandesh.smartguy@gmail.com>
2015-07-01 19:03:32 +0530

    add new component 4 column round icon badge

--- public/index.php
+++ public/index.php
@@ -5,9 +5,8 @@ include('../_includes/menu.html');
 include('../_includes/masthead.html');
 include('../_includes/landing.html');
 include('../_includes/full-banner-a.html');
-include('../_includes/svc-content.html');
-include('../_includes/team-content.html');
-include('../_includes/careers-content.php'); ?>
+include('../_includes/round-icon-badge.html');
+?>
 <div class="sep50"></div>
 <?php
 include('../_includes/contact.html');
