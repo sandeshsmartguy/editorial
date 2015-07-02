89426ec3346ba37af6260a3897b7839ee3d9fae6
sandeshsmartguy <sandesh.smartguy@gmail.com>
2015-07-02 13:10:36 +0530

    miscellaneous update

--- public/index.php
+++ public/index.php
@@ -6,7 +6,7 @@ include('../_includes/masthead.html');
 include('../_includes/landing.html');
 include('../_includes/full-banner-a.html');
 include('../_includes/round-icon-badge.html');
-include('../_includes/content-box-3col.html');
+include('../_includes/left-side-header-panel.html');
 include('../_includes/assignment-examples.html');
 ?>
 <div class="sep50"></div>
