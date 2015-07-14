9654a47431b54e660138eae3535d0069b086f0a3
sandeshsmartguy <sandesh.smartguy@gmail.com>
2015-07-14 16:20:41 +0530

    add popover html in index file

--- public/index.php
+++ public/index.php
@@ -5,6 +5,7 @@ include('../_includes/menu.html');
 include('../_includes/masthead.html');
 include('../_includes/landing.html');
 include('../_includes/full-banner-a.html');
+include('../_includes/popover.html');
 include('../_includes/round-icon-badge.html');
 include('../_includes/left-side-header-panel.html');
 include('../_includes/assignment-examples.html');
