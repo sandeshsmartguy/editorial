5b7fa7aba4780b8903eb1cf1b7fe96441afca3cd
sandeshsmartguy <sandesh.smartguy@gmail.com>
2015-07-14 18:32:08 +0530

    misc update

--- public/index.php
+++ public/index.php
@@ -5,10 +5,10 @@ include('../_includes/menu.html');
 include('../_includes/masthead.html');
 include('../_includes/landing.html');
 include('../_includes/accordian-3col.html');
-//include('../_includes/full-banner-a.html');
-include('../_includes/popover.html');
 include('../_includes/round-icon-badge.html');
-include('../_includes/left-side-header-panel.html');
+include('../_includes/popover.html');
+
+//include('../_includes/left-side-header-panel.html');
 include('../_includes/assignment-examples.html');
 ?>
 <div class="sep50"></div>
