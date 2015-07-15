2dac678214dbbb13a51d3b3b6e9af8d0d683908e
sandeshsmartguy <sandesh.smartguy@gmail.com>
2015-07-15 12:17:18 +0530

    misc update

--- public/index.php
+++ public/index.php
@@ -5,6 +5,7 @@ include('../_includes/menu.html');
 include('../_includes/masthead.html');
 include('../_includes/landing.html');
 include('../_includes/accordian-3col.html');
+//include('../_includes/tab-content.html');
 include('../_includes/round-icon-badge.html');
 include('../_includes/popover.html');
 
